4f496659f0084cac7e0650514c2815c52e234596
MainahMwangi <mainahmwangi12@gmail.com>
2022-07-12 04:32:37 -0700

    interfaces

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	
+	res, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	// fmt.Println(res)
+	// bs := make([]byte, 9999)
+	// res.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	io.Copy(os.Stdout, res.Body)
+}
